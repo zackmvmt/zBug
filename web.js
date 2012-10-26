4fb373b756418c6044c36ad416a37cac35066d0b
Zack <zack@movementstrategy.com>
2012-10-26 16:03:53 -0600

    started working on the cloudant connection

--- web.js
+++ web.js
@@ -2,6 +2,9 @@ var express = require('express');
 
 var app = express.createServer();
 
+var restler = require('restler');
+// figure out how to connect to cloudant
+
 app.get('/', function(req, res) {
 	
 	res.send('Hello World');
