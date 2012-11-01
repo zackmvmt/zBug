358065725665ac3cb65ba55be196d2c44347b309
Zack Stickles <zack@movementstrategy.com>
2012-11-01 15:13:14 -0600

    getting information about the enviromental variables

--- web.js
+++ web.js
@@ -9,8 +9,7 @@ var db = server.use('app8736785.heroku/test01');
 app.get('/', function(req, res) {
 	
 	//var result = server + ' - ' + db + ' - ';
-	//res.send(JSON.stringify(process));
-	res.send(process);
+	res.send(JSON.stringify(process.env));
 	
 });
 
