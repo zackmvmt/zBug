48710109d89543f3c27aa4a19c08494c8949a719
Zack Stickles <zack@movementstrategy.com>
2012-11-01 14:55:39 -0600

    trying to connect to the cloudant database

--- web.js
+++ web.js
@@ -1,18 +1,18 @@
 var express = require('express');
-
 var app = express.createServer();
 
-var restler = require('restler');
-// figure out how to connect to cloudant
+var server = require('nano')('https://app8736785.heroku:IP3yKQ8702okufUmy2vaWu2m@app8736785.heroku.cloudant.com');
+var db = server.use('app8736785.heroku/test01');
 
 app.get('/', function(req, res) {
 	
-	res.send('Hello World');
+	var result = server + ' - ' + db + ' - ';
+	res.send('result');
 	
 });
 
 var port = process.env.PORT || 5000;
 
 app.listen(port);
-console.log('Listening on port', port);
-//console.log('process?', process);
+//console.log('Listening on port', port);
+//console.log('process', process);
