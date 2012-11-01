ca86fb540eb95d342948866a21e0bf1f20234795
Zack Stickles <zack@movementstrategy.com>
2012-11-01 15:16:25 -0600

    added the right database

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer();
 
 var server = require('nano')(process.env.CLOUDANT_URL);
-var db = server.use('app8736785.heroku/test01');
+var db = server.use('test01');
 
 app.get('/', function(req, res) {
 	
