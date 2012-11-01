282abc20d90ece00763c9cf54e3f8d6cf74708e6
Zack Stickles <zack@movementstrategy.com>
2012-11-01 15:20:11 -0600

    trying to get all the information for the server

--- web.js
+++ web.js
@@ -4,12 +4,11 @@ var express = require('express');
 var app = express.createServer();
 
 var server = require('nano')(process.env.CLOUDANT_URL);
-//var db = server.use('test01');
 
 app.get('/', function(req, res) {
 	
 	//var result = server + ' - ' + db + ' - ';
-	//res.send(JSON.stringify(server));
+	res.send(JSON.stringify(server));
 	
 	server.db.list(function(err, body) {
 		res.send(JSON.stringify(body));
