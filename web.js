b47a21839e81cc0ec7cd749b388dacae777d182d
Zack Stickles <zack@movementstrategy.com>
2012-11-01 15:14:41 -0600

    got in the right url, trying to print out the server again

--- web.js
+++ web.js
@@ -3,13 +3,13 @@ var port = process.env.PORT || 5000;
 var express = require('express');
 var app = express.createServer();
 
-var server = require('nano')('https://app8736785.heroku.cloudant.com');
+var server = require('nano')(process.env.CLOUDANT_URL);
 var db = server.use('app8736785.heroku/test01');
 
 app.get('/', function(req, res) {
 	
 	//var result = server + ' - ' + db + ' - ';
-	res.send(JSON.stringify(process.env));
+	res.send(JSON.stringify(server));
 	
 });
 
