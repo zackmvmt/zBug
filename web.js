1ebcae94b2a9f2cf5977932a3dd7545d110118f7
Zack Stickles <zack@movementstrategy.com>
2012-11-01 15:19:01 -0600

    removed db and fix variable name

--- web.js
+++ web.js
@@ -4,14 +4,14 @@ var express = require('express');
 var app = express.createServer();
 
 var server = require('nano')(process.env.CLOUDANT_URL);
-var db = server.use('test01');
+//var db = server.use('test01');
 
 app.get('/', function(req, res) {
 	
 	//var result = server + ' - ' + db + ' - ';
 	//res.send(JSON.stringify(server));
 	
-	nano.db.list(function(err, body) {
+	server.db.list(function(err, body) {
 		res.send(JSON.stringify(body));
 	});
 	
