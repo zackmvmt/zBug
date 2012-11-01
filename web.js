e7c1d99c21c6f8bdbeb14414dbcfbfd4ffa0b089
Zack Stickles <zack@movementstrategy.com>
2012-11-01 15:10:49 -0600

    get the information about the process on the server

--- web.js
+++ web.js
@@ -3,13 +3,13 @@ var port = process.env.PORT || 5000;
 var express = require('express');
 var app = express.createServer();
 
-var server = require('nano')('https://app8736785.heroku.cloudant.com:' + port);
+var server = require('nano')('https://app8736785.heroku.cloudant.com');
 var db = server.use('app8736785.heroku/test01');
 
 app.get('/', function(req, res) {
 	
 	//var result = server + ' - ' + db + ' - ';
-	res.send(JSON.stringify(server));
+	res.send(JSON.stringify(process));
 	
 });
 
