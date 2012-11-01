42d8ee64f1e35c8b000b3c5859873d09972d0ce6
Zack Stickles <zack@movementstrategy.com>
2012-11-01 15:07:00 -0600

    updated the server url

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
+var port = process.env.PORT || 5000;
+
 var express = require('express');
 var app = express.createServer();
 
-var server = require('nano')('https://app8736785.heroku:IP3yKQ8702okufUmy2vaWu2m@app8736785.heroku.cloudant.com');
+var server = require('nano')('https://app8736785.heroku.cloudant.com/');
 var db = server.use('app8736785.heroku/test01');
 
 app.get('/', function(req, res) {
@@ -10,8 +12,6 @@ app.get('/', function(req, res) {
 	res.send(JSON.stringify(server));
 	
 });
-
-var port = process.env.PORT || 5000;
 
 app.listen(port);
 //console.log('Listening on port', port);
