02a36800d70b017297a170909a603be803d24982
Zack Stickles <zack@movementstrategy.com>
2012-11-01 15:08:00 -0600

    added the port to the url

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 5000;
 var express = require('express');
 var app = express.createServer();
 
-var server = require('nano')('https://app8736785.heroku.cloudant.com/');
+var server = require('nano')('https://app8736785.heroku.cloudant.com:' + port);
 var db = server.use('app8736785.heroku/test01');
 
 app.get('/', function(req, res) {
