41db0f22481c43644cc9765d79ebe45052972276
Zack Stickles <zack@movementstrategy.com>
2012-11-02 15:02:03 -0600

    removed uneeded code

--- server.js
+++ server.js
@@ -8,11 +8,6 @@ var app = express.createServer();
 
 // CONFIGURE THE API
 app.configure(function () {
-	/*app.use(function(req, res, next) {
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-		next();
-	});*/
 	app.use(express.static(__dirname + '/public'));
 	app.set('views', __dirname + '/public');
 	app.set('view options', { layout: false });
